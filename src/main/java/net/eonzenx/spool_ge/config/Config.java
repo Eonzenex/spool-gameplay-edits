baa009dc229e3852402d4f6f93c24a0f981209cc
EonZeNx <sparck.va@gmail.com>
2021-08-30 09:46:59 +1200

    New textures - Config file for wings ready

--- src/main/java/net/eonzenx/spool_ge/config/Config.java
+++ src/main/java/net/eonzenx/spool_ge/config/Config.java
@@ -3,8 +3,8 @@ package net.eonzenx.spool_ge.config;
 public class Config
 {
     public final static class Wings {
-        public static final WingSettings Leaf = new WingSettings(0.5f);
-        public static final WingSettings Feather = new WingSettings(0.6f);
-        public static final WingSettings Hide = new WingSettings(0.7f);
+        public static final WingSettings Leaf = new WingSettings(0.55f);
+        public static final WingSettings Feather = new WingSettings(0.7f);
+        public static final WingSettings Hide = new WingSettings(0.85f);
     }
 }
--- src/main/java/net/eonzenx/spool_ge/registry_handlers/ItemRegistry.java
+++ src/main/java/net/eonzenx/spool_ge/registry_handlers/ItemRegistry.java
@@ -15,18 +15,30 @@ public class ItemRegistry
     public static final SGEElytraItem HIDE_WINGS;
     public static final SGEElytraItem ELYTRA_WINGS;
 
+    public static final Item LEAF_WING;
+    public static final Item FEATHER_WING;
+    public static final Item HIDE_WING;
+
 
     public static void init() {
         Registry.register(Registry.ITEM, SpoolGameplayEdits.newId("leaf_wings"), LEAF_WINGS);
         Registry.register(Registry.ITEM, SpoolGameplayEdits.newId("feather_wings"), FEATHER_WINGS);
         Registry.register(Registry.ITEM, SpoolGameplayEdits.newId("hide_wings"), HIDE_WINGS);
         Registry.register(Registry.ITEM, SpoolGameplayEdits.newId("elytra_wings"), ELYTRA_WINGS);
+
+        Registry.register(Registry.ITEM, SpoolGameplayEdits.newId("leaf_wing"), LEAF_WING);
+        Registry.register(Registry.ITEM, SpoolGameplayEdits.newId("feather_wing"), FEATHER_WING);
+        Registry.register(Registry.ITEM, SpoolGameplayEdits.newId("hide_wing"), HIDE_WING);
     }
 
     static {
         LEAF_WINGS = new SGEElytraItem(ToolMaterials.WOOD, new Item.Settings().maxDamage(102).group(ItemGroup.TRANSPORTATION).rarity(Rarity.COMMON));
-        FEATHER_WINGS = new SGEElytraItem(ToolMaterials.STONE, new Item.Settings().maxDamage(215).group(ItemGroup.TRANSPORTATION).rarity(Rarity.UNCOMMON));
+        FEATHER_WINGS = new SGEElytraItem(ToolMaterials.STONE, new Item.Settings().maxDamage(215).group(ItemGroup.TRANSPORTATION).rarity(Rarity.COMMON));
         HIDE_WINGS = new SGEElytraItem(ToolMaterials.IRON, new Item.Settings().maxDamage(322).group(ItemGroup.TRANSPORTATION).rarity(Rarity.UNCOMMON));
         ELYTRA_WINGS = new SGEElytraItem(ToolMaterials.DIAMOND, new Item.Settings().maxDamage(432).group(ItemGroup.TRANSPORTATION).rarity(Rarity.UNCOMMON));
+
+        LEAF_WING = new Item(new Item.Settings().group(ItemGroup.TRANSPORTATION).rarity(Rarity.COMMON).maxCount(2));
+        FEATHER_WING = new Item(new Item.Settings().group(ItemGroup.TRANSPORTATION).rarity(Rarity.COMMON).maxCount(2));
+        HIDE_WING = new Item(new Item.Settings().group(ItemGroup.TRANSPORTATION).rarity(Rarity.COMMON).maxCount(2));
     }
 }
